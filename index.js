80b0ce30a8d207fbba72d4a9ea9103f928cbe1d2
Edgar Mikuro <edgarmikuro@gmail.com>
2025-04-13 10:04:03 +0300

    Completes domContentLoad

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const textEl = document.getElementById("text");
+    textEl.textContent = "This is really cool!";
+});
